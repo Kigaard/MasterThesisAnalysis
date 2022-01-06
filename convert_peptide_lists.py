88d45cfbba213aacfc9ed4857d38e908a1322502
Mads Kierkegaard <mads.k@live.dk>
2022-01-06 11:03:53 +0100

    Clean up code after moving code to package

--- convert_peptide_lists.py
+++ convert_peptide_lists.py
@@ -1,27 +1,5 @@
-import pandas as pd
-import os
-
-import tqdm
-
-
-def convert_peptide_list(peptide_list_file: str):
-    """
-    Convert peptide list file.
-
-    :param peptide_list_file: The peptide list
-    """
-    output_file_name = os.path.splitext(peptide_list_file)[0] + ".xlsx"
-    peptide_list: pd.DataFrame = pd.read_excel(peptide_list_file, engine="xlrd")
-    peptide_list = peptide_list[peptide_list['V'] == 'Y']
-    peptide_list = peptide_list.set_index("spec id")
-    peptide_list.to_excel(output_file_name)
-
+from analysis_code.convert_peptide_list import convert_peptide_list_files
 
 if __name__ == '__main__':
-
-    directory = r""
-    files = [os.path.join(directory, file) for file in os.listdir(directory) if file.endswith(".xls")]
-
-    for file in tqdm.tqdm(files):
-        convert_peptide_list(file)
-
+    peptide_list_directory = r"C:\Users\spec-makie17\Documents\Experiments\211215_Bait_Take3_PH2121_ONGO\MGF"
+    convert_peptide_list_files(peptide_list_directory=peptide_list_directory)
